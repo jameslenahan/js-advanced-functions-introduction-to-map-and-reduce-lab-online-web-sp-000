7a83fdcf52421199414573bd35f423d7424ddb10
James Lenahan <james.k.lenahan@gmail.com>
2020-03-31 17:53:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ function mapToDouble(sourceArray){
 function mapToSquare(sourceArray){
     return sourceArray.map(x => x*x);
 }
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    const reducer = function(accumulator, currentValue){ return accumulator + currentValue }
+    
+    return sourceArray.reduce(reducer, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false)
+}
