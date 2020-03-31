7678d2a13a0188ee2ef9b84b73514d6f030b622e
James Lenahan <james.k.lenahan@gmail.com>
2020-03-31 17:51:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function mapToNegativize(sourceArray){
     return sourceArray.map(x => x * -1);
 }
+function mapToNoChange(sourceArray){
+    return sourceArray.map(x => x);
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(x => x*2);
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(x => x*x);
+}
